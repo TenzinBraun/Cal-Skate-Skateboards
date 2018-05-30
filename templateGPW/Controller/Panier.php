61c054b33ee422e2a348c2e657634f38728883c1
Braun <tenzin.braun@gmail.com>
2018-05-30 14:06:55 +0200

    [add PHP controller + Pictures]

---
+++ templateGPW/Controller/Panier.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 10/04/2018
+ * Time: 01:19
+ */
+
+
+include_once "DBManager.php";
+
+
+
+class Panier
+{
+//Attributes
+   private $numberOfArticleInBasket;
+   private $db;
+
+//Methodes
+
+    public function __construct($numberArticle)
+    {
+        $this->numberOfArticleInBasket = $numberArticle;
+        $this->db = new DBManager();
+    }
+
+    public function addArticleToBasket($idAccount, $idArticle){
+        if(!$this->db->verifyIfArticleIsAddedInBasket($idAccount, $idArticle, "basket")){
+            $this->db->insert2ElementsIntoTable($idAccount,$idArticle,"basket");
+            $this->db++;
+        }
+
+    }
+
+
+}
