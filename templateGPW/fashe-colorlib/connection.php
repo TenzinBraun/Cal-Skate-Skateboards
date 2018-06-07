1e44059e36d4eb527c54fbc3d488366d87998aa7
Braun <tenzin.braun@gmail.com>
2018-06-07 03:11:06 +0200

    [picture] streetwear import

--- templateGPW/fashe-colorlib/connection.php
+++ templateGPW/fashe-colorlib/connection.php
@@ -89,7 +89,7 @@ if(isset($_POST['mail']) && isset($_POST['password'])){
             <select class="selection-1 select2-hidden-accessible" name="time" tabindex="-1" aria-hidden="true">
                 <option>USD</option>
                 <option>EUR</option>
-            </select><span class="select2 select2-container select2-container--default" dir="ltr" style="width: 56.7167px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-time-zx-container"><span class="select2-selection__rendered" id="select2-time-zx-container" title="USD">USD</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+            </select>
         </div>
     </div>
 </div>
