0441cf68df0d8a7a647c6e44a6991fc571de92cd
Leteno <letenothibaudl@gmail.com>
2018-06-07 02:39:09 +0200

    uploadThibaud

---
+++ templateGPW/fashe-colorlib/register.php
@@ -0,0 +1,467 @@
+<?php
+require_once('../Controller/DBManager.php');
+
+$register = "";
+$dbManager = new DBManager();
+if(isset($_POST['firstName']) &&
+    isset($_POST['name']) &&
+    isset($_POST['day']) &&
+    isset($_POST['month']) &&
+    isset($_POST['year']) &&
+    isset($_POST['emailAddress']) &&
+    isset($_POST['password']) &&
+    isset($_POST['confirmPassword'])) {
+    $register = $dbManager->addRegistrationToBD($_POST['firstName'], $_POST['name'], $_POST['day'],
+        $_POST['month'], $_POST['year'], $_POST['emailAddress'], $_POST['password'],
+        $_POST['confirmPassword']);
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cal Skate Skateboards</title>
+    <link rel="stylesheet" href="Bootstrap/CSS/bootstrap.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/themify/themify-icons.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/elegant-font/html-css/style.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
+<body>
+<!---------Navbar------------->
+<div class="topbar">
+    <div class="topbar-social">
+        <a href="#" class="topbar-social-item fa fa-facebook"></a>
+        <a href="#" class="topbar-social-item fa fa-instagram"></a>
+        <a href="#" class="topbar-social-item fa fa-pinterest-p"></a>
+        <a href="#" class="topbar-social-item fa fa-snapchat-ghost"></a>
+        <a href="#" class="topbar-social-item fa fa-youtube-play"></a>
+    </div>
+
+    <span class="topbar-child1">
+					Free shipping for standard order over $100
+				</span>
+
+    <div class="topbar-child2">
+					<span class="topbar-email">
+						fashe@example.com
+					</span>
+
+        <div class="topbar-language rs1-select2">
+            <select class="selection-1 select2-hidden-accessible" name="time" tabindex="-1" aria-hidden="true">
+                <option>USD</option>
+                <option>EUR</option>
+            </select><span class="select2 select2-container select2-container--default" dir="ltr" style="width: 56.7167px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-time-zx-container"><span class="select2-selection__rendered" id="select2-time-zx-container" title="USD">USD</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+        </div>
+    </div>
+</div>
+<div class="wrap_header">
+    <!-- Logo -->
+    <a href="index.html" class="logo">
+        <img src="images/icons/logo.png" alt="IMG-LOGO">
+    </a>
+
+    <!-- Menu -->
+    <div class="wrap_menu">
+        <nav class="menu">
+            <ul class="main_menu">
+                <li>
+                    <a href="index.html">Home</a>
+                    <ul class="sub_menu">
+                        <li><a href="index.html">Homepage V1</a></li>
+                        <li><a href="home-02.html">Homepage V2</a></li>
+                        <li><a href="home-03.html">Homepage V3</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="product.php">Shop</a>
+                </li>
+
+                <li class="sale-noti">
+                    <a href="product.php">Sale</a>
+                </li>
+
+                <li>
+                    <a href="cart.html">Features</a>
+                </li>
+
+                <li>
+                    <a href="blog.html">Blog</a>
+                </li>
+
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <!-- Header Icon -->
+    <div class="header-icons">
+        <a href="connection.php" class="header-wrapicon1 dis-block">
+            <img src="images/icons/icon-header-01.png" class="header-icon1" alt="ICON">
+        </a>
+
+        <span class="linedivide1"></span>
+
+        <div class="header-wrapicon2">
+            <img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+            <span class="header-icons-noti">0</span>
+
+            <!-- Header cart noti -->
+            <div class="header-cart header-dropdown">
+                <ul class="header-cart-wrapitem">
+                    <li class="header-cart-item">
+                        <div class="header-cart-item-img">
+                            <img src="images/item-cart-01.jpg" alt="IMG">
+                        </div>
+
+                        <div class="header-cart-item-txt">
+                            <a href="#" class="header-cart-item-name">
+                                White Shirt With Pleat Detail Back
+                            </a>
+
+                            <span class="header-cart-item-info">
+											1 x $19.00
+										</span>
+                        </div>
+                    </li>
+
+                    <li class="header-cart-item">
+                        <div class="header-cart-item-img">
+                            <img src="images/item-cart-02.jpg" alt="IMG">
+                        </div>
+
+                        <div class="header-cart-item-txt">
+                            <a href="#" class="header-cart-item-name">
+                                Converse All Star Hi Black Canvas
+                            </a>
+
+                            <span class="header-cart-item-info">
+											1 x $39.00
+										</span>
+                        </div>
+                    </li>
+
+                    <li class="header-cart-item">
+                        <div class="header-cart-item-img">
+                            <img src="images/item-cart-03.jpg" alt="IMG">
+                        </div>
+
+                        <div class="header-cart-item-txt">
+                            <a href="#" class="header-cart-item-name">
+                                Nixon Porter Leather Watch In Tan
+                            </a>
+
+                            <span class="header-cart-item-info">
+											1 x $17.00
+										</span>
+                        </div>
+                    </li>
+                </ul>
+
+                <div class="header-cart-total">
+                    Total: $75.00
+                </div>
+
+                <div class="header-cart-buttons">
+                    <div class="header-cart-wrapbtn">
+                        <!-- Button -->
+                        <a href="cart.html" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                            View Cart
+                        </a>
+                    </div>
+
+                    <div class="header-cart-wrapbtn">
+                        <!-- Button -->
+                        <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                            Check Out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!---------Contents---------->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 p-b-30"></div>
+        <div class="col-md-6 p-b-30">
+            <div class="leave-comment" action="" method="post">
+                <h4 class="m-text26 p-b-36 p-t-15">
+                    Create your account now
+                </h4>
+                First Name
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="text" placeholder="First Name" name="firstName" required>
+                </div>
+                Name
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="text" placeholder="Name" name="name" required>
+                </div>
+                Day
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="number" placeholder="Day" name="day"required>
+                </div>
+                Month
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="number" placeholder="Month" name="month" required>
+                </div>
+                Year
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="number" placeholder="Year" name="year" required>
+                </div>
+                Email adresse
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="email" placeholder="Email Address" name="emailAddress" required>
+                </div>
+                Password
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="password" placeholder="Password" name="password" required>
+                </div>
+                Password Confirmation
+                <div class="bo4 of-hidden size15 m-b-20">
+                    <input class="sizefull s-text7 p-l-22 p-r-22" type="password" placeholder="Confirm Password" name="confirmPassword" required>
+                </div>
+                I agree with the term of use<input class="sizefull s-text7 p-l-22 p-r-22" type="checkbox" required>
+                <button type="submit" class="flex-c-m size4 bg7 bo-rad-10 hov1 s-text14 trans-0-4">Create your Account</button>
+            </form>
+        </div>
+    </div>
+</div>
+<!--------Footer-------->
+<footer class="bg6 p-t-30 p-b-43 p-l-45 p-r-45">
+    <div class="flex-w p-b-90">
+        <div class="w-size6 p-t-30 p-l-15 p-r-15 respon3">
+            <h4 class="s-text12 p-b-30">
+                GET IN TOUCH
+            </h4>
+
+            <div>
+                <p class="s-text7 w-size27">
+                    Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+                </p>
+
+                <div class="flex-m p-t-30">
+                    <a href="#" class="fs-18 color1 p-r-20 fa fa-facebook"></a>
+                    <a href="#" class="fs-18 color1 p-r-20 fa fa-instagram"></a>
+                    <a href="#" class="fs-18 color1 p-r-20 fa fa-pinterest-p"></a>
+                    <a href="#" class="fs-18 color1 p-r-20 fa fa-snapchat-ghost"></a>
+                    <a href="#" class="fs-18 color1 p-r-20 fa fa-youtube-play"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+            <h4 class="s-text12 p-b-30">
+                Categories
+            </h4>
+
+            <ul>
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Men
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Women
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Dresses
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Sunglasses
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+            <h4 class="s-text12 p-b-30">
+                Links
+            </h4>
+
+            <ul>
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Search
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        About Us
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Contact Us
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Returns
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+            <h4 class="s-text12 p-b-30">
+                Help
+            </h4>
+
+            <ul>
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Track Order
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Returns
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        Shipping
+                    </a>
+                </li>
+
+                <li class="p-b-9">
+                    <a href="#" class="s-text7">
+                        FAQs
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="w-size8 p-t-30 p-l-15 p-r-15 respon3">
+            <h4 class="s-text12 p-b-30">
+                Newsletter
+            </h4>
+
+            <form>
+                <div class="effect1 w-size9">
+                    <input class="s-text7 bg6 w-full p-b-5" name="email" placeholder="email@example.com" type="text">
+                    <span class="effect1-line"></span>
+                </div>
+
+                <div class="w-size2 p-t-20">
+                    <!-- Button -->
+                    <button class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+                        Subscribe
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <div class="t-center p-l-15 p-r-15">
+        <a href="#">
+            <img class="h-size2" src="images/icons/paypal.png" alt="IMG-PAYPAL">
+        </a>
+
+        <a href="#">
+            <img class="h-size2" src="images/icons/visa.png" alt="IMG-VISA">
+        </a>
+
+        <a href="#">
+            <img class="h-size2" src="images/icons/mastercard.png" alt="IMG-MASTERCARD">
+        </a>
+
+        <a href="#">
+            <img class="h-size2" src="images/icons/express.png" alt="IMG-EXPRESS">
+        </a>
+
+        <a href="#">
+            <img class="h-size2" src="images/icons/discover.png" alt="IMG-DISCOVER">
+        </a>
+
+        <div class="t-center s-text8 p-t-20">
+            Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+        </div>
+    </div>
+</footer>
+
+<!-- Back to top -->
+<div class="btn-back-to-top bg0-hov" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<i class="fa fa-angle-double-up" aria-hidden="true"></i>
+		</span>
+</div>
+
+<!-- Container Selection -->
+<div id="dropDownSelect1"></div>
+<div id="dropDownSelect2"></div>
+
+<!--===============================================================================================-->
+<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
+<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+<script type="text/javascript">
+    $(".selection-1").select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $('#dropDownSelect1')
+    });
+
+    $(".selection-2").select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $('#dropDownSelect2')
+    });
+</script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
